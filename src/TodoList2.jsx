da982901e15ca4936d13100047705b7847550723
Rishijangir566 <rishijangirjr@gmail.com>
2025-01-06 11:19:44 +0530

    reducer usestate

--- src/TodoList2.jsx
+++ src/TodoList2.jsx
@@ -2,28 +2,48 @@ import { useReducer } from 'react'
 
 
 function TodoList2() {
-    const initialState={
-        task:"",
-        Tasks:[]
-    }
-    const [state , dispatch]=useReducer(reducer, initialState)
-    
-    function reducer(state, action ){
-      if (action==="inputTask")return{...state , task:action.payload};
-      else if(action==="addTask")return{...state,Tasks:[...state.task,{id:Date.now(),tasktodo:state.task}]}
-    }
+  const initialState = {
+    task: "",
+    tasks: []
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  function reducer(state, action) {
+    if (action.type === "inputTask") {
+      return { ...state, task: action.payload };
+    } else if (action.type === "addTask") {
+      return {
+        ...state,
+        tasks: [...state.tasks, { id: Date.now(), taskToDo: state.task }],
+      };
+    } else {
+      return state; // Always have a default return for unhandled action types.
+    };
+
+    // switch (action.type) {
+    //   case "inputTask":
+    //     return { ...state, task: action.payload };
+    //   case "addTask":
+    //     return { ...state, tasks: [...state.tasks, { id: Date.now(), taskToDo: state.task }] }
+    // }
+  }
   return (
     <div>
-      <form action="" onClick={(e)=>{e.preventDefault();
-        dispatch({type:"addTask"}) }}>
-        <input type="text" value={state.task} onChange={(e)=>dispatch({type:"inputTask" , payload:e.target.value})} />
-        <button  type='submit'>Click me </button>
+      <form action="" onSubmit={(e) => {
+        e.preventDefault();
+        dispatch({ type: "addTask" })
+      }}>
+        <input type="text" placeholder="Enter Task" value={state.task} onChange={(e) => dispatch({ type: "inputTask", payload: e.target.value })} />
+        <button type="submit">Add Task</button>
       </form>
 
+
       <ul>
-{/* {state.tasks.map((task)=>(<li key={task.id}>{task.tasktodo} </li>))} */}
-{state.tasks.map((task)=>(<li key={task.id}>{task.taskToDo} </li>))}
+        {state.tasks.map((task) => (
+          <li key={task.id}>{task.taskToDo} </li>
+        ))}
       </ul>
+      
     </div>
   )
 }
